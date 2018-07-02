3b15a177da5f016a56c186995c7aa0b77278a759
PRIYOTOSH <34639967+PRIYOTOSH-DEV@users.noreply.github.com>
2018-07-02 15:45:23 +0530

    Add files via upload

---
+++ API/foodcategory.php
@@ -0,0 +1,10 @@
+<?php
+	require_once "foodcategoryscript.php";
+	$foodcategory_object=new foodCategory();
+	if(isset($_GET["restaurant_id"]) && !empty("restaurant_id")){
+			$restaurant_id=$_GET["restaurant_id"];
+			$json_array=$foodcategory_object->getfoodcategory($restaurant_id);
+			echo json_encode($json_array);
+		}
+	
+?>
