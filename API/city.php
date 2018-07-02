715f93ea8347e83b96893549ac34817617f30c18
Partha Pratim Sarma <perthxrma@hotmail.com>
2018-07-02 11:45:50 +0530

    Update city.php

--- API/city.php
+++ API/city.php
@@ -1,13 +1,6 @@
 <?php
-	require_once "connect.php";
-	$dbObject= new DbConnect();
-	$query="SELECT city_name,city_address FROM city";
-	$result=mysqli_query($dbObject->getDb(),$query);
-	$json_array=array();
-	while($row=mysqli_fetch_assoc($result)){
-		$json_array[]=$row;
-	}
-
-	$json_city=json_encode($json_array);
-	echo $json_city;
+	require_once 'city_script.php';
+	$city_object=new City();
+	$json_array=$city_object->getAll();
+	echo json_encode($json_array);
 ?>
