3e56ef93db8d337c939bef43c71c058bf572c2b2
PRIYOTOSH <34639967+PRIYOTOSH-DEV@users.noreply.github.com>
2018-07-03 12:40:57 +0530

    Update fooditems.php

--- API/fooditems.php
+++ API/fooditems.php
@@ -1,9 +1,9 @@
 <?php
 	require_once "fooditems_script.php";
 	$foodItems_object=new foodItems();
-	if(isset($_GET["food_category"]) && !empty("food_category")){
-			$food_category=$_GET["food_category"];
-			$json_array=$foodItems_object->getfoodItems($food_category);
+	if(isset($_GET["foodcategory_id"]) && !empty("foodcategory_id")){
+			$foodcategory_id=$_GET["foodcategory_id"];
+			$json_array=$foodItems_object->getfoodItems($foodcategory_id);
 			echo json_encode($json_array);
 		}
 	
