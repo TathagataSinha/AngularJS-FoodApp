6a7fc36649dba931bf014175e3e6986c773d5137
PRIYOTOSH <34639967+PRIYOTOSH-DEV@users.noreply.github.com>
2018-07-04 16:58:11 +0530

    Add files via upload

---
+++ API/foodcategory_script.php
@@ -0,0 +1,49 @@
+<?php
+	include_once "connect.php";
+	class foodCategory{
+		private $db;
+		private $db_table="food_category";
+		public function __construct(){
+			$this->db=new Dbconnect();
+		}
+		public function getAll(){
+			$query=" SELECT * FROM ".$this->db_table;
+			$result=mysqli_query($this->db->getDb(),$query);
+			if($result){
+				$json["success"]=true;
+				$json_array=array();
+				while($row=mysqli_fetch_assoc($result)){
+					$json_array[]=$row;
+				}
+				$json["message"]=$json_array;
+				$json["data"]="";
+				return $json;
+				}
+				else{
+					$json["success"]=false;
+					$json["message"]="";
+					$json["data"]="";
+				}
+		}
+		public function getfoodcategory($restaurant_id){
+			$query=" SELECT * FROM ".$this->db_table." WHERE restaurant_id='$restaurant_id'";
+			$result=mysqli_query($this->db->getDb(),$query);
+			if($result){
+				$json["success"]=true;
+				$json_array=array();
+				while($row=mysqli_fetch_assoc($result)){
+					$json_array[]=$row;
+				}
+				$json["message"]=$json_array;
+				$json["data"]="";
+				return $json;
+				}
+				else{
+					$json["success"]=false;
+					$json["message"]="";
+					$json["data"]="";
+				}
+			}
+	}
+
+?>
---
+++ API/restaurant_script.php
@@ -0,0 +1,30 @@
+<?php
+	include_once "connect.php";
+	class restaurant{
+		private $db;
+		public function __construct(){
+			$this->db=new Dbconnect();
+		}
+		public function getrestaurant($restaurant_id){
+			$query=" SELECT * FROM restaurant LEFT JOIN restaurant_photos ON restaurant.restaurant_id = restaurant_photos.restaurant_id WHERE restaurant.restaurant_id='$restaurant_id'";
+			$result=mysqli_query($this->db->getDb(),$query);
+			if($result){
+			$json["success"]=true;
+			$json_array=array();
+			while($row=mysqli_fetch_assoc($result)){
+				$json_array[]=$row;
+			}
+			$json["message"]=$json_array;
+			$json["data"]="";
+		}
+			else{
+			$json["status"]=false;
+			$json["message"]="";
+			$json["data"]="";
+	}
+			return $json;
+
+	}
+	}
+
+?>
