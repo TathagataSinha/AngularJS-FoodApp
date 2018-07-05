c423ebc30094234f0b4f2cdcf8178392d2cba9b8
Partha Pratim Sarma <perthxrma@hotmail.com>
2018-07-05 17:09:09 +0530

    Update restaurant.php

--- API/restaurant.php
+++ API/restaurant.php
@@ -1,9 +1,9 @@
 <?php
 	require_once "restaurant_script.php";
 	$restaurant_object=new restaurant();
-	if(isset($_GET["restaurant_id"]) && !empty("restaurant_id")){
-			$restaurant_id=$_GET["restaurant_id"];
-			$json_array=$restaurant_object->getrestaurant($restaurant_id);
+	if(isset($_POST["mall_id"]) && !empty("mall_id")){
+			$mall_id=$_POST["mall_id"];
+			$json_array=$restaurant_object->getrestaurant($mall_id);
 			echo json_encode($json_array);
 		}
 	
