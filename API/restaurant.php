8c7b721d05a6e315ccbfa39ff952b43bd9766686
Partha Pratim Sarma <perthxrma@hotmail.com>
2018-07-05 17:25:17 +0530

    Update restaurant.php

--- API/restaurant.php
+++ API/restaurant.php
@@ -4,7 +4,7 @@
 		$mall_id=$POST_['mall_id'];
 	}
 	$restaurant_object=new restaurant();
-	if(isset($_POST["mall_id"]) && !empty("mall_id")){
+	if(!empty($mall_id)){
 			$mall_id=$_POST["mall_id"];
 			$json_array=$restaurant_object->getrestaurant($mall_id);
 			echo json_encode($json_array);
