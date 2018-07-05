106bd976ac6cdda8f235d4b561a8583852891a32
Partha Pratim Sarma <perthxrma@hotmail.com>
2018-07-05 17:07:14 +0530

    Update restaurant_script.php

--- API/restaurant_script.php
+++ API/restaurant_script.php
@@ -5,26 +5,26 @@
 		public function __construct(){
 			$this->db=new Dbconnect();
 		}
-		public function getrestaurant($restaurant_id){
-			$query=" SELECT * FROM restaurant LEFT JOIN restaurant_photos ON restaurant.restaurant_id = restaurant_photos.restaurant_id WHERE restaurant.restaurant_id='$restaurant_id'";
+		public function getrestaurant($mall_id){
+			$query=" SELECT * FROM restaurant LEFT JOIN restaurant_photos ON restaurant.restaurant_id = restaurant_photos.restaurant_id WHERE restaurant.mall_id='$mall_id'";
 			$result=mysqli_query($this->db->getDb(),$query);
 			if($result){
-			$json["success"]=true;
-			$json_array=array();
-			while($row=mysqli_fetch_assoc($result)){
-				$json_array[]=$row;
+				$json["success"]=true;
+				$json_array=array();
+				while($row=mysqli_fetch_assoc($result)){
+					$json_array[]=$row;
+				}
+				$json["message"]=$json_array;
+				$json["data"]="";
 			}
-			$json["message"]=$json_array;
-			$json["data"]="";
-		}
 			else{
-			$json["status"]=false;
-			$json["message"]="";
-			$json["data"]="";
-	}
+				$json["status"]=false;
+				$json["message"]="";
+				$json["data"]="";
+			}
 			return $json;
 
-	}
+		}
 	}
 
 ?>
