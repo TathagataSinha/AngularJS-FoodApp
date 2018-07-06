b958f03eb6fcc4c66e526c228f6a236f3f315c7f
Partha Pratim Sarma <perthxrma@hotmail.com>
2018-07-06 15:32:10 +0530

    Update foodcategory.php

--- API/foodcategory.php
+++ API/foodcategory.php
@@ -1,10 +1,12 @@
 <?php
 	require_once "foodcategory_script.php";
+	if(isset($_POST['restaurant_id']){
+		$restaurant_id=$_POST['restaurant_id'];	
+	}
 	$foodcategory_object=new foodCategory();
-	if(isset($_GET["restaurant_id"]) && !empty("restaurant_id")){
-			$restaurant_id=$_GET["restaurant_id"];
-			$json_array=$foodcategory_object->getfoodcategory($restaurant_id);
-			echo json_encode($json_array);
-		}
+	if(!empty($restaurant_id)){
+		$json_array=$foodcategory_object->getfoodcategory($restaurant_id);
+		echo json_encode($json_array);
+	}
 	
 ?>
