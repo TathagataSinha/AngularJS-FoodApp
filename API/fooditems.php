656f1d2d60848e679eb3b2f68d74f72e401a6a33
Partha Pratim Sarma <perthxrma@hotmail.com>
2018-07-06 15:35:12 +0530

    Update fooditems.php

--- API/fooditems.php
+++ API/fooditems.php
@@ -1,10 +1,11 @@
 <?php
 	require_once "fooditems_script.php";
+	if(isset($_POST['foodcategory_id'])){
+		$foodcategory_id=$_POST['foodcategory_id'];
+	}
 	$foodItems_object=new foodItems();
-	if(isset($_GET["foodcategory_id"]) && !empty("foodcategory_id")){
-			$foodcategory_id=$_GET["foodcategory_id"];
-			$json_array=$foodItems_object->getfoodItems($foodcategory_id);
-			echo json_encode($json_array);
-		}
-	
+	if(!empty($foodcategory_id)){
+		$json_array=$foodItems_object->getfoodItems($foodcategory_id);
+		echo json_encode($json_array);
+	}	
 ?>
